ff75c28eaa43503b04a015b6abace0b075080093
Geisy Domiciano <geisydomiciano@gmail.com>
2019-09-14 16:32:01 +0200

    :sparkles: add main game functionality

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-{
-  alert('hi')()
+const CHOICES = ['rock', 'paper', 'scissors']
+
+let computerChoice
+let userChoice
+let isUserWinner
+
+const submit = function () {
+  computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)]
+  userChoice = document.querySelector('.radio:checked').value
+  checkWinner()
+  console.log(userChoice, computerChoice, isUserWinner)
+}
+
+const checkWinner = () => {
+  if (userChoice === 'rock' && computerChoice === 'scissor') {
+    isUserWinner = true
+  } else if (userChoice === 'paper' && computerChoice === 'rock') {
+    isUserWinner = true
+  } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+    isUserWinner = true
+  } else if (userChoice === computerChoice) {
+    isUserWinner = false
+  } else {
+    isUserWinner = false
+  }
+  return isUserWinner
 }
