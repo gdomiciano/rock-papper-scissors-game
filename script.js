a4dd035697eff1654ae4ba9c080112d19cc43a62
Geisy Domiciano <geisydomiciano@gmail.com>
2019-12-04 19:58:01 +0100

    :construction: add vue structure

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-const CHOICES = ['rock', 'paper', 'scissors']
+const CHOICES = ['rock', 'paper', 'scissors'];
 
-let computerChoice
-let userChoice
-let isUserWinner
+let computerChoice;
+let userChoice;
+let isUserWinner;
 
 const submit = function () {
-  computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)]
-  userChoice = document.querySelector('.radio:checked').value
-  checkWinner()
-  console.log(userChoice, computerChoice, isUserWinner)
-  showResult()
-}
+  computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+  userChoice = document.querySelector('.radio:checked').value;
+  checkWinner();
+  console.log(userChoice, computerChoice, isUserWinner);
+  showResult();
+};
 
 const checkWinner = () => {
   if (userChoice === 'rock' && computerChoice === 'scissor') {
-    isUserWinner = true
+    isUserWinner = true;
   } else if (userChoice === 'paper' && computerChoice === 'rock') {
-    isUserWinner = true
+    isUserWinner = true;
   } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-    isUserWinner = true
+    isUserWinner = true;
   } else if (userChoice === computerChoice) {
-    isUserWinner = false
+    isUserWinner = false;
   } else {
-    isUserWinner = false
+    isUserWinner = false;
   }
-  return isUserWinner
-}
+  return isUserWinner;
+};
 
 const showResult = function () {
   switch (isUserWinner) {
     case true:
-      document.querySelector('.winner').style.display = 'block'
-      document.querySelector('.looser').style.display = 'none'
-      break
+      document.querySelector('.winner').style.display = 'block';
+      document.querySelector('.looser').style.display = 'none';
+      break;
     case false:
-      document.querySelector('.winner').style.display = 'none'
-      document.querySelector('.looser').style.display = 'block'
-      break
+      document.querySelector('.winner').style.display = 'none';
+      document.querySelector('.looser').style.display = 'block';
+      break;
     default:
-      document.querySelector('.winner').style.display = 'none'
-      document.querySelector('.looser').style.display = 'none'
-      break
+      document.querySelector('.winner').style.display = 'none';
+      document.querySelector('.looser').style.display = 'none';
+      break;
   }
-}
+};
