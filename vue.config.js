4457d96d2f862d1f6d0c3c3e1104c335889401e2
Geisy Domiciano <geisydomiciano@gmail.com>
2019-12-07 16:33:17 +0100

    :rocket: add deploy config & deploy

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// https://gdomiciano.github.io/rock-papper-scissors-game/
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/rock-papper-scissors-game/'
+    : '/'
+}
