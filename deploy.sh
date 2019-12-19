c7805faf477e96359098ef7062c08c9a7c5c3c88
Geisy Domiciano <geisydomiciano@gmail.com>
2019-12-19 04:07:35 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:gdomiciano/rock-papper-scissors-game.git master:gh-pages
 
 cd -
