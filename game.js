5227e2cd7885c0608b3d663302172eb9323e738e
Geisy Domiciano <geisy.domiciano@telenor.se>
2020-04-12 12:06:12 +0200

    :tada: add start point for JS

---
+++ game.js
@@ -0,0 +1,12 @@
+// array with valid choices
+const CHOICES = ['rock', 'paper', 'scissors']; 
+
+let computerChoice;
+
+// function to be called by the form on the html
+const submit = function () { 
+    /* randomly choose one of the 3 valid choices and assign it to the @computerChoice variable
+     * ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+     */
+    computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)]; 
+}
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-// https://gdomiciano.github.io/rock-papper-scissors-game/
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/rock-papper-scissors-game/'
-    : '/'
-}
